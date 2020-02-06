dd965ceb176cbcaa4b9411015b51add4f92f6ac9
jerry_chen <jerry_chen@gemteks.com>
2020-02-06 15:35:12 +0800

    1. if in_file is a folder, ignore it. 2. if strtok return null,  skip the while()

--- name_re.c
+++ name_re.c
@@ -10,18 +10,19 @@ char cut1[] = " - PressPlay 訂閱學習，時刻精進";
 int main(){
 
 	FILE* fp;
-	char name[1024];
+	char name[512];
 	char *pch;
 	int i,j;
 	char cutstring[8][128];
 	char buff[1024];
+	char fullname[1024];
 	memset(cutstring,'\0',sizeof(cutstring[0][0])*8*128);
 	
     DIR* FD;
     struct dirent* in_file;
 	char in_dir[]="/home/jerrychen/Downloads/FireShot/150_size";
-    /* Openiing common file for writing */
-
+   
+	/* Openiing folder, to get the pdf file */
     if (NULL == (FD = opendir (in_dir))) 
     {
         fprintf(stderr, "Error : Failed to open input directory - %s\n", strerror(errno));
@@ -33,23 +34,29 @@ int main(){
             continue;
         if (!strcmp (in_file->d_name, ".."))    
             continue;
+        if (in_file->d_type != 8)    /*if the file_type is not regular file */
+            continue;
 
 		strcpy(name,in_file->d_name);
 		sprintf(buff,"%s/%s",in_dir,name);
-		pch = strstr(name ,cut1);		
-		*pch = '\0';	
-		strcat(name,".pdf");
+		
+		while((pch = strstr(name ,cut1))!=NULL)
+		{
+			*pch = '\0';
+			strcat(name,".pdf");
+		}
 		for(i=0;i<strlen(name)-1;i++)
 		{
-			if(name[i] =="｜"[0] && name[i+1] =="｜"[1] && name[i+2] =="｜"[2])
+			if(name[i] =="｜"[0] && name[i+1] =="｜"[1] && name[i+2] =="｜"[2])   /*for mutiple character(chineses), it consists of 3 ch*/
 			{
 				name[i]=' ';
 				name[i+1]='|';
 				name[i+2]=' ';
 			}
 		}
-		pch=strtok(name,". ");
+
 		i=0;
+		pch=strtok(name,". ");
 		while(pch!=NULL)
 		{
 			strcpy(cutstring[i++],pch);
@@ -61,11 +68,11 @@ int main(){
 			strcat(name,cutstring[j]);
 		}
 		//printf("%s\n",name);
-		char newname[1024];
-		sprintf(newname,"%s/%s",in_dir,name);
-		printf("%s\n",newname);
 
-		if(rename(buff,newname)!=0)
+		sprintf(fullname,"%s/%s",in_dir,name);
+		printf("%s\n",fullname);
+
+		if(rename(buff,fullname)!=0)
 		{
 			fprintf(stderr, "Error : Failed to change file [%s] - %s\n", buff,strerror(errno));
 			return -1;
